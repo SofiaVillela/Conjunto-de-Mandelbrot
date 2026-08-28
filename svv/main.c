bdfab4fa3454639d3d0b8641a86f0ede5180641a
Sofia <sofiavillelav@gmail.com>
2026-08-28 16:43:01 -0300

    fix: valida formato numerico dos argumentos (atoi aceitava texto invalido silenciosamente)

--- svv/main.c
+++ svv/main.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-enum { LARGURA, ALTURA, MAX_INTERACOES ,NUM_THREADS};
+enum { LARGURA, ALTURA, MAX_ITERACOES ,NUM_THREADS};
 
 int main(int argc, char **argv){
     if(argc != 5){
         fprintf(stderr, "erro, quantidade insuficiente de argumentos.\n");
         exit(1);
     }
-    
+    char *endptr;
     int array_numeros[4];
     for(int i = 0, j = 1; i < 4; i++, j++){
-        array_numeros[i] = atoi(argv[j]);
+        array_numeros[i] =  strtol(argv[j], &endptr, 10);
+        if(*endptr != '\0'){
+            fprintf(stderr, "teste de erro de caracter diferente de numero\n");
+            exit(1);
+        }
         if(array_numeros[i] <= 0){
-            printf("teste menor/igual a 0: %d", array_numeros[i]);
+            fprintf(stderr,"menor/igual a 0: %d\n", array_numeros[i]);
             exit(1);
         }
     }
